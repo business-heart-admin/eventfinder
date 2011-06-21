6b439eea359a00143320c61b3605a92a44d6f818
Unni Vimal <unnivimal@gmail.com>
2011-06-21 14:52:22 -0700

    Added code to allow for trainer & venue might already exist when creating the event

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,8 +3,7 @@ import org.businessheart.*
 
 class BootStrap {
 
-	def init = {
-		servletContext ->
+	def init = { servletContext ->
 		log.info "-" * 40
 		log.info "running bootstrap"
 		log.info "-" * 40
@@ -12,58 +11,64 @@ class BootStrap {
 	}
 	def destroy = {
 	}
-	
-	
-	private void sampleData(){
-		Trainer trainer = new Trainer();
-		trainer.firstName = "Joe"
-		trainer.lastName = "Smith"
-		trainer.email = "joe.smith@example.com"
-		trainer.displayGravatar = true
-		trainer.phone = "9999999"
-		trainer.address1 = "235"
-		trainer.address2 = "Example"
-		trainer.city = "Irvine"
-		trainer.state = "CA"
-		trainer.country = "USA"
-		trainer.zip = "999"
-		trainer.organization = "Conscires"
-		trainer.website = "http://www.example.com"
-		trainer.certifications = "CSM"
-		trainer.active = true
-		trainer.keywords = "Scrum"
-		trainer.comments = "NA"
 
-		try{
-			trainer.save(failOnError:true)
+
+	private void sampleData(){
+		Trainer trainer = Trainer.findByEmail("joe.smith@example.com")
+		if(!trainer){
+			trainer = new Trainer();
+			trainer.firstName = "Joe"
+			trainer.lastName = "Smith"
+			trainer.email = "joe.smith@example.com"
+			trainer.displayGravatar = true
+			trainer.phone = "9999999"
+			trainer.address1 = "235"
+			trainer.address2 = "Example"
+			trainer.city = "Irvine"
+			trainer.state = "CA"
+			trainer.country = "USA"
+			trainer.zip = "999"
+			trainer.organization = "Conscires"
+			trainer.website = "http://www.example.com"
+			trainer.certifications = "CSM"
+			trainer.active = true
+			trainer.keywords = "Scrum"
+			trainer.comments = "NA"
+
+			try{
+				trainer.save(failOnError:true)
+			}
+			catch(grails.validation.ValidationException e){
+				//do nothing
+			}
 		}
-		catch(Exception e){
-			//do nothing
-		}
-		
+
 		log.trace("Enter Sample Venue")
-		Venue venue = new Venue();
-		venue.name = "RC Global"
-		venue.address1 = "Greco Aisle"
-		venue.address2 = "bbhsabj"
-		venue.city = "Irvine"
-		venue.state = "California"
-		venue.country = "USA"
-		venue.zip = "92614"
-		venue.contactName = "John"
-		venue.contactPhone = "56798809"
-		venue.capacity = 1000
-		venue.trainerComments = "Test"
-		venue.studentComments = "Test"
-				
-		try{
-			venue.save(failOnError:true)
+		Venue venue = Venue.findByName("RC Global")
+		if(!venue){
+			venue = new Venue();
+			venue.name = "RC Global"
+			venue.address1 = "Greco Aisle"
+			venue.address2 = "bbhsabj"
+			venue.city = "Irvine"
+			venue.state = "California"
+			venue.country = "USA"
+			venue.zip = "92614"
+			venue.contactName = "John"
+			venue.contactPhone = "56798809"
+			venue.capacity = 1000
+			venue.trainerComments = "Test"
+			venue.studentComments = "Test"
+
+			try{
+				venue.save(failOnError:true)
+			}
+			catch(grails.validation.ValidationException e){
+				//do nothing
+			}
 		}
-		catch(Exception e){
-			//do nothing
-		}
-		
-	
+
+
 		log.trace("Enter Sample Event")
 		Event event = new Event();
 		event.name = "Agile"
@@ -76,14 +81,12 @@ class BootStrap {
 		event.level = "Beginner"
 		event.trainerComments = "Test"
 		event.studentComments = "Test"
-		
+
 		try{
 			event.save(failOnError:true)
 		}
-		catch(Exception e){
+		catch(grails.validation.ValidationException e){
 			//do nothing
 		}
-	
 	}
-
 }

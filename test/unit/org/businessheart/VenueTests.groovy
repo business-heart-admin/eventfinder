4c5593050e94cbda288791eda36f8c720b14e2f9
Unni Vimal <unnivimal@gmail.com>
2011-06-28 14:25:27 -0700

    Added test code for Venue

--- test/unit/org/businessheart/VenueTests.groovy
+++ test/unit/org/businessheart/VenueTests.groovy
@@ -5,7 +5,7 @@ import grails.test.*
 class VenueTests extends GrailsUnitTestCase {
 	protected void setUp() {
 		super.setUp()
-		mockDomain(Trainer)
+		mockDomain(Venue)
 	}
 
 	protected void tearDown() {
@@ -13,118 +13,60 @@ class VenueTests extends GrailsUnitTestCase {
 	}
 
 	void testValidate_Empty() {
-		def trainer = new Trainer()
-		assertFalse trainer.validate()
-		assertTrue trainer.hasErrors()
-		assertEquals 15,trainer.errors.errorCount
-		assertEquals "nullable",trainer.errors["phone"]
+		def venue = new Venue()
+		assertFalse venue.validate()
+		assertTrue venue.hasErrors()
+		assertEquals 10,venue.errors.errorCount
+		//venue.errors.each {println it}
+		assertEquals "nullable",venue.errors["contactPhone"]
 	}
 
 	void testValidate_Blank() {
-		def trainer = new Trainer()
-		trainer.phone = ''
-		trainer.lastName = ''
-		trainer.firstName = ''
-		trainer.email = ''
-		trainer.displayGravatar = false
-		trainer.address1 = ''
-		trainer.address2 = ''
-		trainer.city = ''
-		trainer.state = ''
-		trainer.country = ''
-		trainer.zip = ''
-		trainer.organization = ''
-		trainer.website = ''
-		trainer.certifications = ''
-		trainer.comments = ''
-		trainer.keywords = ''
-		assertFalse trainer.validate()
-		assertTrue trainer.hasErrors()
-		assertEquals 7,trainer.errors.errorCount
-		//trainer.errors.each {println it}
-		assertEquals "blank",trainer.errors["phone"]
+		def venue = new Venue()
+		venue.name = ''
+		venue.address1 = ''
+		venue.address2 = ''
+		venue.city = ''
+		venue.state = ''
+		venue.country = ''
+		venue.zip = ''
+		venue.contactName = ''
+		venue.contactPhone = ''
+		venue.capacity = 0
+		venue.trainerComments = ''
+		venue.studentComments = ''
+		assertFalse venue.validate()
+		assertTrue venue.hasErrors()
+		assertEquals 5,venue.errors.errorCount
+		//enue.errors.each {println it}
+		assertEquals "blank",venue.errors["city"]
 	}
 
 	void testValidate_Good() {
-		def trainer = new Trainer()
-		trainer.phone = '9946555257'
-		trainer.lastName = 'Smith'
-		trainer.firstName = 'Joe'
-		trainer.email = 'joe@example.com'
-		trainer.displayGravatar = false
-		trainer.address1 = '1 Greco'
-		trainer.address2 = ''
-		trainer.city = 'Anytown'
-		trainer.state = 'CA'
-		trainer.country = 'USA'
-		trainer.zip = '99999'
-		trainer.organization = ''
-		trainer.website = ''
-		trainer.certifications = ''
-		trainer.comments = ''
-		trainer.keywords = ''
-		assertTrue trainer.validate()
-		assertFalse trainer.hasErrors()
-		assertEquals 0,trainer.errors.errorCount
-		//trainer.errors.each {println it}
+		def venue = new Venue()
+		venue.name = 'Conscires'
+		venue.address1 = '1 Greco Aisle'
+		venue.address2 = ''
+		venue.city = 'Irvine'
+		venue.state = 'CA'
+		venue.country = 'USA'
+		venue.zip = ''
+		venue.contactName = ''
+		venue.contactPhone = ''
+		venue.capacity = 10
+		venue.trainerComments = ''
+		venue.studentComments = ''
+		assertTrue venue.validate()
+		assertFalse venue.hasErrors()
+		assertEquals 0,venue.errors.errorCount
+		//venue.errors.each {println it}
 	}
-	void testValidate_BadEmail() {
-		def trainer = new Trainer()
-		trainer.phone = '9946555257'
-		trainer.lastName = 'Smith'
-		trainer.firstName = 'Joe'
-		trainer.email = 'joeexample.com'
-		trainer.displayGravatar = false
-		trainer.address1 = '1 Greco'
-		trainer.address2 = ''
-		trainer.city = 'Anytown'
-		trainer.state = 'CA'
-		trainer.country = 'USA'
-		trainer.zip = '99999'
-		trainer.organization = ''
-		trainer.website = ''
-		trainer.certifications = ''
-		trainer.comments = ''
-		trainer.keywords = ''
-		assertFalse trainer.validate()
-		assertTrue trainer.hasErrors()
-		assertEquals 1,trainer.errors.errorCount
-		//trainer.errors.each {println it}
-		assertEquals "email",trainer.errors["email"]
-	}
-	
+		
 	public void testToString() throws Exception {
-		def trainer = new Trainer()
-		trainer.lastName = 'Smith'
-		trainer.firstName = 'Joe'
-		trainer.email = 'joeexample.com'
-		assertEquals 'Smith, Joe (joeexample.com)',trainer.toString()
+		def venue = new Venue()
+		venue.name = 'Conscires'
+		assertEquals 'Conscires',venue.toString()
 		
 	}
 
-	void testValidate_BadUrl() {
-		def trainer = new Trainer()
-		trainer.phone = '9946555257'
-		trainer.lastName = 'Smith'
-		trainer.firstName = 'Joe'
-		trainer.email = 'joe@example.com'
-		trainer.displayGravatar = false
-		trainer.address1 = '1 Greco'
-		trainer.address2 = ''
-		trainer.city = 'Anytown'
-		trainer.state = 'CA'
-		trainer.country = 'USA'
-		trainer.zip = '99999'
-		trainer.organization = ''
-		trainer.website = 'www.conscires.com'
-		trainer.certifications = ''
-		trainer.comments = ''
-		trainer.keywords = ''
-		assertFalse trainer.validate()
-		assertTrue trainer.hasErrors()
-		assertEquals 1,trainer.errors.errorCount
-		//trainer.errors.each {println it}
-		assertEquals "url",trainer.errors["website"]
-	}
-	
 }
